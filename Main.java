1677b0eba5199f241708f9a9be097d9ae675ca81
Abdou Karim Cissokho <akatech@outlook.fr>
2021-12-12 13:49:26 +0000

    add new line

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main{
     public static void main(String[] args) {
         System.out.println("Hello, Cissokho");
+        System.out.println("Hello, Cissokho Test push - build automatique");
     }
 }
